0c78e8c11a82fdc028df8f1cae11eab21a27652b
jimmy <jimmy@jimmy.jimmy>
2024-11-03 11:01:36 -0500

    reworked some of the database org

--- database/database.py
+++ database/database.py
@@ -3,26 +3,19 @@ from firebase_admin import firestore
 from firebase_admin import credentials
 from google.cloud.firestore_v1 import CollectionReference, DocumentReference, FieldFilter
 
-from constellation import constellation, star
+# from constellation import constellation, star
 
 class Database:
     def __init__(self):
-        cred = credentials.Certificate("./database/key.json")
+        cred = credentials.Certificate("./key.json")
         firebase_admin.initialize_app(cred)
         self.db = firestore.client()
 
     def get_constellations_col(self):
         return self.db.collection("constellations")
     
-    def get_stars_col(self) -> CollectionReference:
-        return self.db.collection("stars")
-    
     def find_const(self, name):
-        return self.get_constellations_col().where(filter=FieldFilter())
-
-    def find_star(self, star_name):
-        col = self.get_stars_col().document(star_name)
-        return col.get() if col.get().exists else None
+        return self.get_constellations_col().stream()
         
     def get_constellation(self, name):
         pass
@@ -37,6 +30,6 @@ class Database:
 
 if __name__ == '__main__':
     d = Database()
-    x = d.find_const("cassiopeia")
-
-    print(x)
+    x = d.get_constellations_col().stream()
+    for y in x:
+        print(y)
