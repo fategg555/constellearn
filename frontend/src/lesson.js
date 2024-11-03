35a05a383a6a79fe31f1551561b70302f39cdc70
Daniel Zhang <69740332+D123aniel@users.noreply.github.com>
2024-11-03 10:58:23 -0500

    lessons boxes and text input

--- frontend/src/lesson.js
+++ frontend/src/lesson.js
@@ -20,8 +20,12 @@ function Lesson({name="name", starPattern}) {
                     <Star top={"300px"} left={"270px"} />
                     <Star top={"360px"} left={"120px"} />
                 </div>
+                <section class="infoBox">
+                    <h2> Read Text: Lorem ipsum odor amet, consectetuer adipiscing elit. Primis himenaeos ullamcorper ornare ultricies efficitur mollis fames. Ac nunc in ridiculus potenti urna eu. Lacinia turpis lacus eleifend ornare scelerisque. Ligula elementum semper suscipit cras commodo purus erat. Finibus iaculis tellus cursus finibus sapien dolor dignissim. Praesent malesuada gravida leo montes feugiat nam amet mollis. Amet nec nisl molestie augue senectus faucibus. Semper etiam torquent laoreet risus sodales etiam volutpat porttitor.</h2>
+                    <h3> Question below:  Lorem ipsum odor amet, consectetuer adipiscing elit. Primis himenaeos ullamcorper ornare ultricies efficitur mollis fames.?</h3>
+                </section>
                 <div class="progress">
-
+                    <textarea id="constText" name = "constText"></textarea>
                 </div>
             </div>
         </div>
