90b0762d32b12c6562b5e3f3cc84e12c2cb9d94b
Aidan yung <aidany@ad.unc.edu>
2024-11-03 00:17:00 -0400

    thumbnailsgit add -Agit add -A

--- frontend/src/thumbnail.js
+++ frontend/src/thumbnail.js
@@ -3,7 +3,12 @@ import React from 'react';
 function Thumbnail() {
     return (
         <div class="thumbnail">
-            
+            <div class="name roboto-light">
+                <h2> CASSIOPEIA </h2>
+            </div>
+            <div class="rating">
+                <p> DIFFICULTY: MEDIUM </p>
+            </div>
         </div>
     )
 }
