b97f740e4072cef27985575dd5a8c96712551121
Aidan yung <aidany@ad.unc.edu>
2024-11-03 10:40:59 -0500

    gogogo

--- frontend/src/lesson.js
+++ frontend/src/lesson.js
@@ -3,24 +3,24 @@ import Star from './star.js';
 
 const test = [0, 0, 0, 1, 0];
 
-function Lesson() {
+function Lesson({name="name", starPattern}) {
     return (
-        <div class="inf">
+        <div class="info">
             <div class="lesson">
                 <div class="data"> 
-                    <h1> NAME </h1>
+                    <h1> {name} </h1>
                 </div>
                 <div class="constellation">
                     {/* row 1 */}
-                    <Star top={"1vh"} left={"40vw"} />
-                    <Star top={"8vh"} left={"31vw"} />
-                    <Star top={"19vh"} left={"26vw"} /> 
-                    <Star top={"29vh"} left={"22vw"} />
-                    <Star top={"39vh"} left={"12vw"} />
-                    <Star top={"47vh"} left={"20vw"} />
-                    <Star top={"55vh"} left={"8vw"} />
+                    <Star top={"10px"} left={"510px"} />
+                    <Star top={"60px"} left={"380px"} />
+                    <Star top={"130px"} left={"300px"} /> 
+                    <Star top={"200px"} left={"290px"} />
+                    <Star top={"270px"} left={"150px"} />
+                    <Star top={"300px"} left={"270px"} />
+                    <Star top={"360px"} left={"120px"} />
                 </div>
-                <div>
+                <div class="progress">
 
                 </div>
             </div>
